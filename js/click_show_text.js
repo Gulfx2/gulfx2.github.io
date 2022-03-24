f3708f6c96f2e5376d96b45d4630ef0468d1843a
gulfx2 <a565405671@gmail.com>
2022-03-24 12:56:40 +0800

    Site updated: 2022-03-24 12:56:40

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -1,24 +1,24 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
-    $("body").click(function(e) {
+    $(“body”).click(function(e) {
         var a = new Array
-        ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
-        var $i = $("<span/>").text(a[a_idx]);
+        (“富强”, “民主”, “文明”, “和谐”, “自由”, “平等”, “公正”, “法治”, “爱国”, “敬业”, “诚信”, “友善”);
+        var $i = $(“<span/>”).text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
         y = e.pageY;
         $i.css({
-            "z-index": 5,
-            "top": y - 20,
-            "left": x,
-            "position": "absolute",
-            "font-weight": "bold",
-            "color": "#FF0000"
+            “z-index”: 5,
+            “top”: y - 20,
+            “left”: x,
+            “position”: “absolute”,
+            “font-weight”: “bold”,
+            “color”: “#FF0000”
         });
-        $("body").append($i);
+        $(“body”).append($i);
         $i.animate({
-            "top": y - 180,
-            "opacity": 0
+            “top”: y - 180,
+            “opacity”: 0
         },
             3000,
             function() {
@@ -29,5 +29,5 @@ jQuery(document).ready(function($) {
 });
 
 function delay() {
-    $(".buryit").removeAttr("onclick");
+    $(“.buryit”).removeAttr(“onclick”);
 }
